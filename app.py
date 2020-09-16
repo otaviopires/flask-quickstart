aff4391293667d3348ed67c7ea2ba3fff0379f99
Otavio Pires <otavio@arpiatecnologia.com>
2020-09-16 13:43:51 -0300

    unique urls redirection behavior

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
 @app.route('/user/<username>')
 def show_user_profile(username):
     # show the user profile for that user
