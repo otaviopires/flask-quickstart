4ae1950f2fd6f66c990cc3e48e320b0fc86fce67
Otavio Pires <otavio@arpiatecnologia.com>
2020-09-16 13:39:46 -0300

    variable rules

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 from flask import Flask
+from markupsafe import escape
+
 app = Flask(__name__)
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % escape(username)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post %d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    return 'Subpath %s ' % escape(subpath)
 
 @app.route('/')
 def index():
